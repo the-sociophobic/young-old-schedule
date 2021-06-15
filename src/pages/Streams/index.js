0ad526e0a47d94fa62ca81df425fa6ece8c92639
the_sociophobic <torikowashi@gmail.com>
2021-06-15 19:26:27 +0300

    add cover-img to streams

--- src/pages/Streams/index.js
+++ src/pages/Streams/index.js
@@ -14,6 +14,8 @@ import countable from 'utils/countable'
 import clockLike from 'utils/clockLike'
 import Video from 'components/Video'
 
+import cover from './covers/young old cover 2021.jpg'
+
 
 const videos = [
   {
@@ -22,7 +24,7 @@ const videos = [
     link: "//ok.ru/videoembed/2765743138256",
     title: "Забота рядом: лучшие практики конкурса «Ближний круг»",
     // place: "Одноклассники",
-    // cover: cover0,
+    cover: 'https://sun9-8.userapi.com/impg/LZeP7-8LmA9ZPEk01p7rAN3N8ecvy5uUcmyhfA/c7S6uKyqe-M.jpg?size=1140x570&quality=96&sign=a0c4054e3645bf80904840a4cf43a8f4&type=album',
   },
   {
     date: 26,
@@ -30,21 +32,23 @@ const videos = [
     link: "//ok.ru/videoembed/2770121336272",
     title: "О соседских сообществах: разговор с Ириной Кондрашиной, ТОС «Высота»",
     // place: "Площадка",
-    // cover: cover1,
+    cover: 'https://sun9-8.userapi.com/impg/LZeP7-8LmA9ZPEk01p7rAN3N8ecvy5uUcmyhfA/c7S6uKyqe-M.jpg?size=1140x570&quality=96&sign=a0c4054e3645bf80904840a4cf43a8f4&type=album',
   },
   {
     date: 31,
     time: "18:00",
-    // link: "https://www.youtube.com/embed/D7kjsAjIUH0",
-    title: "Практическая мастерская «Как построить сообщество вокруг себя»",
+    link: "//ok.ru/videoembed/2782754448848",
+    title: "Как найти «невидимых» пожилых и начать помогать им? Прямой эфир с Надеждой Калякиной, проект «Забытые живые»",
     // place: "Zoom",
+    cover: 'https://sun9-8.userapi.com/impg/LZeP7-8LmA9ZPEk01p7rAN3N8ecvy5uUcmyhfA/c7S6uKyqe-M.jpg?size=1140x570&quality=96&sign=a0c4054e3645bf80904840a4cf43a8f4&type=album',
   },
   {
     date: 3,
     time: "18:00",
-    // link: "https://www.youtube.com/embed/D7kjsAjIUH0",
+    link: "//ok.ru/videoembed/2789295334864",
     title: "Как конструктивно общаться с пожилыми? Эфир с писателем Александром Галицким",
     // place: "Zoom",
+    cover: 'https://sun9-8.userapi.com/impg/LZeP7-8LmA9ZPEk01p7rAN3N8ecvy5uUcmyhfA/c7S6uKyqe-M.jpg?size=1140x570&quality=96&sign=a0c4054e3645bf80904840a4cf43a8f4&type=album',
   },
 ]
 
